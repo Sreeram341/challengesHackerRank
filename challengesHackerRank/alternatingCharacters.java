8409d87c6f217d8e143944ddfb27f4e242e0a2ff
Sreeram341 <pulavarthis@gmail.com>
2018-10-02 20:59:25 -0500

    some challenge

--- challengesHackerRank/alternatingCharacters.java
+++ challengesHackerRank/alternatingCharacters.java
@@ -1,34 +1,37 @@
-import java.util.Arrays;
+package challengesHackerRank;
+
+import java.util.*;
 
 public class alternatingCharacters {
 
 	static int alternatingCharacters(String s) {
 		
-		String [] splitString = s.split("");
-		stringCounterMap scm = new stringCounterMap();
-		
-		Arrays.stream(splitString).forEach(e->{
-			scm.addOrReplace(e);
-		});
-		
-		int finMin=0; // Wrong
-		
-		for(int min:scm.values()) {
-			if (min<finMin) {
-				finMin =min;
+		String [] splitString = s.split("");		
+		Set<String> adjElem = new HashSet<String>(2);
+		int cntr=0;
+		for (int i=0;i<splitString.length;i++) {
+			if(!adjElem.contains(splitString[i]) && adjElem.size()<2) {
+				adjElem.add(splitString[i]);
 			}
-		}
-		
-		System.out.println(finMin);
-		
-		return 0;
+			else if(adjElem.size()==2) {adjElem.clear();adjElem.add(splitString[i]);}
+			//System.out.println("After clear and add "+adjElem);
+			else  {
+				cntr+=1;
+			}
+		} 
+		if (adjElem.size()==1)cntr+=1;
+		return cntr;
     }
 	
 	public static void main(String[] args) {
 		
-		String s= "ABABABBC";
-		
+		String s= "AAABBBAABB";
+		//String s ="AABBCT";
+		//String s ="BABA";
+		//String s= "AAAAA";
 		int result = alternatingCharacters(s);
+		
+		System.out.println(result);
 		
 	}
 
