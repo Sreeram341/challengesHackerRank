8719c38e95152ded34c925558685ee5bdd4b5242
SreeramPulavarthi <pulavarthis@gmail.com>
2018-10-05 16:48:11 -0500

    Update specialPalindromeAgain.java

--- challengesHackerRank/specialPalindromeAgain.java
+++ challengesHackerRank/specialPalindromeAgain.java
@@ -3,22 +3,39 @@ package challengesHackerRank;
 import java.util.*;
 
 public class specialPalindromeAgain {
-	static List<String> combinationList = new ArrayList<String>();
+
+	static List<StringBuilder> combinationList = new ArrayList<StringBuilder>();
+	static List<String[]> oldcombinationList = new ArrayList<String[]>();
 	
 	static long substrCount(int n, String s) {
 		long retrncntr=0;
+		generateCombinations("-",s);
 		
-		generateCombinations("",s);
-		
-		System.out.println(combinationList);
-
+		for (String[] allEntries:oldcombinationList) {
+			System.out.println(Arrays.toString(allEntries));
+			int leng = allEntries.length;
+			int mid =-1;
+			if(leng%2==0) {mid = leng/2;leng--;}
+			else {mid=(leng/2)+1;leng--;}
+			outerloop:
+			for(int i=0;i<=leng;i++){
+				//Set checkUniq = new HashSet (Arrays.asList(allEntries));
+				//System.out.print(Arrays.toString(allEntries));System.out.print("--"+checkUniq);
+				if(i==mid)break outerloop;
+				//System.out.format("%s,%s,%s\n",allEntries[i],"--",allEntries[leng-i]);
+				else if(allEntries[i]==allEntries[leng-i]) {
+					cntr++;
+				}else {
+					break outerloop;
+				}
+			}
+		}
 		return retrncntr;
     }
-	
-	 static void generateCombinations(String prefix, String s) {  	
 		 
-		 for (int i = 0; i < s.length(); i++) generateCombinations(prefix + s.charAt(i), s.substring(i + 1));
-		 combinationList.add(prefix);
+	 static void generateCombinations(String prefix, String s) {
+		 for (int i = 0; i < s.length(); i++) generateCombinations(prefix + s.charAt(i), s.substring(i + 1));		
+		 oldcombinationList.add(prefix.replace("-", "").split(""));
 	 } 
 	
 	public static void main(String[] args) {
